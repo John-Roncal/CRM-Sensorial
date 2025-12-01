10b31810a6713d0b5c17da7f37c7a2604b948fe3
John Albert Roncal Castillo <johnroncal.19.jarc@gmail.com>
2025-12-01 08:30:05 -0500

    Hola

--- AppWebCentralRestaurante/Controllers/ChatController.cs
+++ AppWebCentralRestaurante/Controllers/ChatController.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AppWebCentralRestaurante.Controllers
 {
+    [Authorize]
     public class ChatController : Controller
     {
         // GET /Chat
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             return View(); // Views/Chat/Index.cshtml
         }
